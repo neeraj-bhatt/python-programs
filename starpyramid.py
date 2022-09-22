22e572a0da43e4b9f8729315dba061e620e8ef18
neerajbhatt <neerajbhatt264@gmail.com>
2022-09-22 19:25:21 +0530

    pattern corrected

--- starpyramid.py
+++ starpyramid.py
@@ -1,21 +1,19 @@
-#This program takes value from user and print a pyramid pattern
+#This program takes value from user and print a pattern
 
-
-n = int(input("Enter a number : "))
 
 #upper pyramid
+n = int(input("Enter a Number : "))
 for i in range(0, n):
-    for k in range(0, n-i-1):
+    for j in range(0, n-i-1):
         print(" ", end=" ")
-    for j in range(0, i+1):
-        print("* ", end="  ")
+    for k in range(0, 2*i+1):
+        print("* ", end="")
     print("")
-    
+
 #lower pyramid
-for a in range(0, n-1):
-    for b in range(0, a+1):
+for o in range(0, n):
+    for b in range(0, 2*n-1+o):
         print(" ", end=" ")
-    for c in range(0, n-a-1):
-        print("* ", end="  ")
+    for c in range(0, 2*(n-o-1)+1):
+        print("* ", end="")
     print("")
-    
